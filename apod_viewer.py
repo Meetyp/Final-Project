f3ff0d98f94a0696d8494fbcb79f1ff92d434ac4
Meetyp <meetyogeshkumar.patel@flemingcollege.ca>
2023-03-25 20:25:14 -0400

    Update apod_viewer.py

--- apod_viewer.py
+++ apod_viewer.py
@@ -7,15 +7,12 @@ import image_lib
 from PIL import Image, ImageTk
 from tkcalendar import DateEntry
 
-image = Image.open("Logo.png")
-image_size = image_lib.scale_image((1000, 500))
-resize_image = image.resize(image_size)
-print(resize_image)
 
 # Determine the path and parent directory of this script
 script_path = os.path.abspath(inspect.getframeinfo(inspect.currentframe()).filename)
 script_dir = os.path.dirname(script_path)
 icon_path = os.path.join(script_dir, 'Icon.ico')
+database_file = 'APOD/image_cache.db'
 
 # Initialize the image cache
 apod_desktop.init_apod_cache(script_dir)
@@ -24,58 +21,98 @@ apod_desktop.init_apod_cache(script_dir)
 root = Tk()
 root.title("Astronomy Picture of the Day Viewer")
 root.iconbitmap(icon_path)
-root.geometry('900x500')
+root.geometry('900x600')
 
 date_var = StringVar()
 explanation_var = StringVar()
 title_var = StringVar()
+image_frame = Frame(root)
+image_frame.pack(fill=BOTH, expand=True)
 
 
-def get_date():
+image = Image.open('Logo.png')
+image_size = image_lib.scale_image((1000, 500))
+resize_image = image.resize(image_size)
+
+img = ImageTk.PhotoImage(resize_image)
+show_image_label = Label(image_frame, image=img)
+show_image_label.image = img
+show_image_label.pack()
+
+
+def setting_image(image_path):
+    image1 = Image.open(image_path)
+    image_size1 = image_lib.scale_image((1000, 500))
+    resize_image1 = image1.resize(image_size1)
+
+    new_img = ImageTk.PhotoImage(resize_image1)
+    show_image_label.configure(image=new_img)
+    show_image_label.image = new_img
+
+
+def get_data():
+    global date_var
+    date_var = calender.get_date()
+    record_id = apod_desktop.add_apod_to_cache(date_var)
+    print(f"RECORD ID: {record_id}")
+    apod_information = apod_desktop.get_apod_info(record_id)
+    print(apod_information)
+    image_lib.set_desktop_background_image(apod_information['file_path'])
+    setting_image(apod_information['file_path'])
+    image_explanation_label['text'] = apod_information['explanation']
+    print(f"Combobox Current ID: {image_list_combobox.current()}")
+
+
+def get_data_from_db():
+    record_id = image_list_combobox.current() + 1
+    print(f"RECORD ID: {record_id}")
+    apod_information = apod_desktop.get_apod_info(record_id)
+    print(apod_information)
+    image_lib.set_desktop_background_image(apod_information['file_path'])
+    setting_image(apod_information['file_path'])
+    image_explanation_label['text'] = apod_information['explanation']
+    print(f"Combobox Current ID: {image_list_combobox.current()}")
+
+
+def download_image():
     global date_var
     date_var = calender.get_date()
 
 
 def handle_combobox(e):
-    global title_var
-    print(title_var.get())
     set_as_desktop_btn['state'] = "active"
 
 
-img = ImageTk.PhotoImage(resize_image)
-show_image_label = Label(image=img)
-show_image_label.image = img
-show_image_label.pack()
+image_explanation_label = Label(image_frame, text="", justify=LEFT)
+image_explanation_label.pack(fill=X, padx=5, pady=5)
+image_explanation_label.bind('<Configure>', lambda e: image_explanation_label.config(wraplength=image_explanation_label.winfo_width()))
 
-image_explanation_label = Label(root, text="Select Image: ")
-image_explanation_label.pack()
-
-select_image_frame = LabelFrame(root, text='View Cached Image')
-select_image_frame.place(x=10, y=430, width=450)
+select_image_frame = LabelFrame(image_frame, text='View Cached Image')
+select_image_frame.pack(side=LEFT, expand=True, fill=X, anchor=S, padx=5)
 
 select_image_label = Label(select_image_frame, text="Select Image: ")
-select_image_label.grid(row=0, column=0, pady=5, ipady=2)
+select_image_label.pack(side=LEFT, padx=5, pady=5)
 
 image_list = apod_desktop.get_all_apod_titles()
 image_list_combobox = ttk.Combobox(select_image_frame, values=image_list, state='readonly', textvariable=title_var)
 image_list_combobox.set("Select an image")
-image_list_combobox.grid(row=0, column=1, pady=5)
+image_list_combobox.pack(side=LEFT, padx=5, pady=5)
 image_list_combobox.bind('<<ComboboxSelected>>', handle_combobox)
 
-set_as_desktop_btn = Button(select_image_frame, text="Set as Desktop", state=DISABLED)
-set_as_desktop_btn.grid(row=0, column=2, pady=5, ipady=1, padx=2)
+set_as_desktop_btn = Button(select_image_frame, text="Set as Desktop", state=DISABLED, command=lambda: get_data_from_db())
+set_as_desktop_btn.pack(side=LEFT, padx=5, pady=5)
 
 # Get More Images
-get_image_frame = LabelFrame(root, text='Get More Images')
-get_image_frame.place(x=470, y=430, width=450)
+get_image_frame = LabelFrame(image_frame, text='Get More Images')
+get_image_frame.pack(side=RIGHT, expand=True, fill=X, anchor=S, padx=5)
 
 get_image_label = Label(get_image_frame, text="Select Date: ")
-get_image_label.grid(row=0, column=0, pady=5, ipady=2)
+get_image_label.pack(side=LEFT, padx=5, pady=5)
 
 calender = DateEntry(get_image_frame, selectmode='day')
-calender.grid(row=0, column=1, pady=5)
+calender.pack(side=LEFT, padx=5, pady=5)
 
-download_image_btn = Button(get_image_frame, text="Download Image", command=lambda:get_date())
-download_image_btn.grid(row=0, column=2, pady=5, ipady=1, padx=2)
+download_image_btn = Button(get_image_frame, text="Download Image", command=lambda: get_data())
+download_image_btn.pack(side=LEFT, padx=5, pady=5)
 
 root.mainloop()
